683270c99b2c8bd63d2b5c3936b65748438daf4d
zkaans <zk_senol@outlook.com>
2026-04-10 21:09:30 +0300

    code file was loaded

---
+++ code_1.py
@@ -0,0 +1,4 @@
+Python 3.12.11 (main, Jun  7 2025, 12:31:34)  [GCC UCRT 15.1.0 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> # First line of your first code
+... print("Hello world!")
